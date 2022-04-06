7037be2d6ca0da02e017427905cba8f70f448e4e
VanishMax <maxkors2014@gmail.com>
2022-04-06 16:00:08 +0300

    Fix pragma

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './style.css';
-import App from './components/markup';
+import App from './components/app';
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
--- src/pragma.ts
+++ src/pragma.ts
@@ -1,18 +1,15 @@
-interface Attributes {
-  [key: string]: string | number | null | undefined;
-}
+type Tag = string | ((props: any, children: any[]) => JSX.Element);
+type Props = Record<string, string | number | null | undefined> | null;
+type Children = (Node | string)[];
 
-const createElement = (
-  tag: string | ((props: any, children: any[]) => JSX.Element),
-  props: Attributes | null,
-  ...children: (Node | string)[][]
-) => {
+export const h = (tag: Tag, props: Props, ...children: Children) => {
+  // If the tag is a function component, pass props and children inside it
   if (typeof tag === 'function') {
     return tag({ ... props }, children);
   }
 
+  // Create the element and add attributes to it
   const el = document.createElement(tag);
-
   if (props) {
     Object.entries(props).forEach(([key, val]) => {
       if (key === 'className') {
@@ -24,15 +21,12 @@ const createElement = (
     });
   }
 
+  // Append all children to the element
   children.forEach((child) => {
-    el.append(...child);
+    el.append(child);
   });
 
   return el;
-};
-
-export const h = (type: string, props: Attributes | null, ...children: any) => {
-  return createElement(type, props, children);
 };
 
 export const Fragment = () => {
